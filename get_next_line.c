18bb56a4f5c53b32143f24557678e6004b4f3ce5
alex44 <sergeyevnasa@gmail.com>
2026-05-15 16:42:05 +0300

    final code

--- get_next_line.c
+++ get_next_line.c
@@ -17,13 +17,58 @@ int conditioned_strlen(char *buff)
     }
     return (i);
 }
-char *get_next_line(int fd)
+
+static char *cleanup_on_error(char *buff, char **storage)
+{
+    free(buff);
+    free(*storage);
+    *storage = NULL;
+    return (NULL);
+}
+
+static char *return_remaining_storage(char *buff, char **storage)
+{
+    char *tmp;
+
+    free(buff);
+    if (*storage)
+    {
+        tmp = *storage;
+        *storage = NULL;
+        return (tmp);
+    }
+    return (NULL);
+}
+
+static char *append_buffer(char *storage, char *buff)
+{
+    char *tmp;
+
+    if (!storage)
+        return (ft_strdup(buff));
+    tmp = ft_strjoin(storage, buff);
+    free(storage);
+    return (tmp);
+}
+
+static char *extract_line(char **storage)
 {
     char *tmp;
     char *line;
+    int len;
+
+    tmp = *storage;
+    len = conditioned_strlen(tmp);
+    *storage = ft_substr(tmp, len, ft_strlen(tmp));
+    line = ft_substr(tmp, 0, len);
+    free(tmp);
+    return (line);
+}
+
+char *get_next_line(int fd)
+{
     char *buff;
     int readed;
-    int len;
     static char *storage;
 
     if ((fd < 0) || BUFFER_SIZE < 1)
@@ -31,48 +76,17 @@ char *get_next_line(int fd)
     buff = malloc(BUFFER_SIZE + 1);
     if (!buff)
         return (NULL);
-    readed = 1;
     while (1)
     {
         readed = read(fd, buff, BUFFER_SIZE);
         if (readed == -1)
-        {
-        free(buff);
-        free(storage);      
-        storage = NULL;
-        return (NULL);
-        }
+            return (cleanup_on_error(buff, &storage));
         if (readed == 0)
-        {
-            free(buff);
-            if (storage)
-            {
-                tmp = storage;
-                storage = NULL;
-                return tmp;
-            }
-            return (NULL);
-        }
+            return (return_remaining_storage(buff, &storage));
         buff[readed] = '\0';
-        if (!storage)
-        {
-            storage = ft_strdup(buff);
-        }
-        else
-        {
-            tmp = ft_strjoin(storage, buff);
-            free(storage);
-            storage = tmp;
-        }
+        storage = append_buffer(storage, buff);
         if (storage && ft_strchr(storage, '\n'))
-        {
-            tmp = storage;
-            len = conditioned_strlen(tmp);
-            storage = ft_substr(tmp, len, ft_strlen(tmp));
-            line = ft_substr(tmp, 0, len);
-            free(tmp);
-            return line;
-        }
+            return (extract_line(&storage));
     }
     return (NULL);
 }
